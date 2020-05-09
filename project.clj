b6f62a8f4a1fad71a56cce16f116e53731590195
Husain Al-Mohssen <husain@alum.mit.edu>
2020-05-09 13:41:02 -0400

    removed un-needed dependency

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
    :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [clj-http "3.10.1"]
+;                 [clj-http "3.10.1"]
                  [clj-http-lite "0.3.0"]
                  [cljsjs/hls "0.12.2-0"]
                  [org.clojure/clojurescript "1.10.597"]

2fc1509bffd3b3e9cf726d1c0e2e282aefe0f251
Husain Al-Mohssen <husain@alum.mit.edu>
2020-05-09 01:19:31 -0400

    adding proxy code to allow server from datastore without copying over to docker container

--- project.clj
+++ project.clj
@@ -1,21 +1,27 @@
 (defproject co-video "0.1.0-SNAPSHOT"
-  :description "Co-Video a util to share HLS video streams across the internet and keep them in  sync" 
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description
+  "Co-Video a util to share HLS video streams across the internet and keep them in  sync"
+  :license
+  {:name "Eclipse Public License"
+   :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.10.1"]
+                 [clj-http "3.10.1"]
+                 [clj-http-lite "0.3.0"]
                  [cljsjs/hls "0.12.2-0"]
-	       	 [org.clojure/clojurescript "1.10.597"]
+                 [org.clojure/clojurescript "1.10.597"]
                  [compojure "1.6.1"]
                  [http-kit "2.3.0"]
                  [cljs-http "0.1.46"]
                  [reagent "0.10.0"]]
   :main co-video.webserver
 
-  :plugins [[lein-cljsbuild "1.1.7"]
-            [lein-figwheel "0.5.19"]]
+  :plugins
+  [[lein-cljsbuild "1.1.7"]
+   [lein-figwheel "0.5.19"]]
 
-  :clean-targets ^{:protect false}
+  :clean-targets
+  ^{:protect false}
 
   [:target-path
    [:cljsbuild :builds :app :compiler :output-dir]
@@ -23,39 +29,42 @@
 
   :resource-paths ["public"]
 
-  :figwheel {:http-server-root "."
-             :nrepl-port 7002
-             :nrepl-middleware [cider.piggieback/wrap-cljs-repl]
-             :css-dirs ["public/css"]}
+  :figwheel
+  {:http-server-root "."
+   :nrepl-port       7002
+   :nrepl-middleware [cider.piggieback/wrap-cljs-repl]
+   :css-dirs         ["public/css"]}
 
-  :cljsbuild {:builds {:app
-                       {:source-paths ["src" "env/dev/cljs"]
-                        :compiler
-                        {:main "co-video.dev"
-                         :output-to "public/js/app.js"
-                         :output-dir "public/js/out"
-                         :asset-path   "js/out"
-                         :source-map true
-                         :optimizations :none
-                         :pretty-print  true}
-                        :figwheel
-                        {:on-jsload "co-video.core/mount-root"
-;                         :open-urls ["http://localhost:3449/index.html"]}}
-                         :open-urls ["http://localhost/index.html"]}}
+  :cljsbuild
+  {:builds {:app
+            {:source-paths ["src" "env/dev/cljs"]
+             :compiler
+             {:main          "co-video.dev"
+              :output-to     "public/js/app.js"
+              :output-dir    "public/js/out"
+              :asset-path    "js/out"
+              :source-map    true
+              :optimizations :none
+              :pretty-print  true}
+             :figwheel
+             {:on-jsload "co-video.core/mount-root"
+              ;                         :open-urls ["http://localhost:3449/index.html"]}}
+              :open-urls ["http://localhost/index.html"]}}
 
-                       :release
-                       {:source-paths ["src" "env/prod/cljs"]
-                        :compiler
-                        {:output-to "public/js/app.js"
-                         :output-dir "target/release"
-                         :optimizations :advanced
-                         :infer-externs true
-                         :pretty-print false}}}}
+            :release
+            {:source-paths ["src" "env/prod/cljs"]
+             :compiler
+             {:output-to     "public/js/app.js"
+              :output-dir    "target/release"
+              :optimizations :advanced
+              :infer-externs true
+              :pretty-print  false}}}}
 
   :aliases {"package" ["do" "clean" ["cljsbuild" "once" "release"]]}
 
-  :profiles {:dev {:source-paths ["src" "env/dev/clj"]
-                   :dependencies [[binaryage/devtools "1.0.0"]
-                                  [figwheel-sidecar "0.5.19"]
-                                  [nrepl "0.6.0"]
-                                  [cider/piggieback "0.4.2"]]}})
+  :profiles
+  {:dev {:source-paths ["src" "env/dev/clj"]
+         :dependencies [[binaryage/devtools "1.0.0"]
+                        [figwheel-sidecar "0.5.19"]
+                        [nrepl "0.6.0"]
+                        [cider/piggieback "0.4.2"]]}})
--- src/co_video/webserver.clj
+++ src/co_video/webserver.clj
@@ -1,61 +1,77 @@
 (ns co-video.webserver
-  (:require [compojure.core :refer :all]
-            [compojure.route :as route]
-            [clojure.spec.alpha :as s]
-            [ring.middleware.cors :refer [wrap-cors]]
-            [org.httpkit.server :refer [run-server]]))
+    (:require [compojure.core :refer :all]
+              [clj-http.lite.client :as client]
+              [compojure.route :as route]
+              [clojure.spec.alpha :as s]
+              [ring.middleware.cors :refer [wrap-cors]]
+              [org.httpkit.server :refer [run-server]]))
 
 ; half a DB in two statements:)
 (def db (atom {}))
+
+; hack to allow this server to proxy a data store like s3 or gs
+(def data-store-url (clojure.string/replace (slurp "data-store-url") "\n" ""))
+
 (add-watch db :watcher-log
            (fn [key atom old-state new-state]
              (if (not= old-state new-state)
                (do
-;                 (println "-- DB Changed --\t[[" key "]] watch log:")
-;                 (println "old-state" old-state)
-;                 (clojure.pprint/pprint (str "atom" @atom))
-;                 (println "new-state" new-state)
+                 ;                 (println "-- DB Changed --\t[[" key "]] watch log:")
+                 ;                 (println "old-state" old-state)
+                 ;                 (clojure.pprint/pprint (str "atom" @atom))
+                 ;                 (println "new-state" new-state)
                  (spit "/tmp/db.edn" new-state)))))
 
-(def uuid-regexp #"[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}")
+(def uuid-regexp
+  #"[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}")
 
 ; New state spec:
-(comment {:uuid         "822bcc0e-4161-44bd-866c-9a21defe38ad"
-          ; no need for an atom here
-          :url          "/vids/300.m3u8"
-          :play?        true
-          :time-seconds 757.322
-          :update-task-num nil
-          })
+(comment
+ {:uuid            "822bcc0e-4161-44bd-866c-9a21defe38ad"
+  ; no need for an atom here
+  :url             "/vids/300.m3u8"
+  :play?           true
+  :time-seconds    757.322
+  :update-task-num nil})
 
 (s/def ::uuid #(re-matches uuid-regexp %))
 (s/def ::url string?)
 (s/def ::play? boolean?)
 (s/def ::time-seconds number?)
 (s/def ::update-task-num (s/or :nothing nil? :task-num int?))
-(s/def ::new-state (s/keys :req-un [::uuid ::url ::play? ::time-seconds ::update-task-num]))
+(s/def ::new-state
+       (s/keys :req-un [::uuid ::url ::play? ::time-seconds ::update-task-num]))
 
 
 (defn valid? [state] (s/valid? ::new-state state))
 
 (defn update-state-n-log [req]
-  (let [new-state (clojure.edn/read-string (slurp (:body req)))
+  (let [new-state       (clojure.edn/read-string (slurp (:body req)))
         valid-new-state (valid? new-state)
-        uuid (:uuid new-state)]
+        uuid            (:uuid new-state)]
     (assert valid-new-state)
     (reset! db (merge @db {(:uuid new-state) new-state}))
-;    (clojure.pprint/pprint (get @db uuid))
+    ;    (clojure.pprint/pprint (get @db uuid))
     {:status  200
      :headers {"Content-Type" "text/html"}
      :body    (get @db uuid)}))
 
+(defn get-file [filename]
+  (let [url (str data-store-url filename)]
+    (println (str "pulling in the file:" filename "\t fetching url: " url))
+    (client/get url {:as :stream :throw-exceptions false})))
 
 (defroutes my-blog
   (GET "/state/:uuid" [uuid]
        (println (str "GET /state/" uuid))
-       (println (str "=" (get @db uuid{:error true})))
-       (str (get  @db uuid {:error true})))
+       (println (str "=" (get @db uuid {:error true})))
+       (str (get @db uuid {:error true})))
+  (GET "/vid/:a{.*}"
+       [a]
+       (get-file a))
+
   (POST "/update-state" [req] update-state-n-log)
+
   (route/files "/")
   (route/not-found "<h1> dead end </h1>"))
 
